696b380ee11a09a5f8d30df60dae345d8d449a73
wlsh44 <k87913j@gmail.com>
2023-02-15 21:40:27 +0900

    fix: json 매핑 오류 수정 [#1]

--- src/main/java/com/example/sns/auth/application/dto/OAuthUserInfoDto.java
+++ src/main/java/com/example/sns/auth/application/dto/OAuthUserInfoDto.java
@@ -1,9 +1,11 @@
 package com.example.sns.auth.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OAuthUserInfoDto {
 
     private String name;
