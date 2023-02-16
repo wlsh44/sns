660ee5b37dd370b742444e63d618b92c8cd3faf7
wlsh44 <k87913j@gmail.com>
2023-02-16 09:58:54 +0900

    test: jwtProvider 테스트 코드 추가 [#1]

---
+++ src/test/java/com/example/sns/auth/infrastructure/JwtProviderTest.java
@@ -0,0 +1,60 @@
+package com.example.sns.auth.infrastructure;
+
+import com.example.sns.auth.exception.ExpiredTokenException;
+import com.example.sns.auth.exception.InvalidTokenException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class JwtProviderTest {
+
+    JwtProvider provider;
+    String secretKey;
+    String issuer;
+
+    @BeforeEach
+    void init() {
+        secretKey = "secret-key secret-key secret-key secret-key secret-key secret-key secret-key";
+        issuer = "test";
+    }
+
+    @Test
+    @DisplayName("토큰 생성 및 추출")
+    void create() throws Exception {
+        //given
+        provider = new JwtProvider(secretKey, 10000L, issuer);
+        String token = provider.createToken(1L);
+
+        //when
+        Long userId = provider.parseUserId(token);
+
+        //then
+        assertThat(userId).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("토큰 기간 만료")
+    void getUserId() throws Exception {
+        //given
+        provider = new JwtProvider(secretKey, 0, issuer);
+        String token = provider.createToken(1L);
+
+        //when then
+        assertThatThrownBy(() -> provider.parseUserId(token))
+                .isInstanceOf(ExpiredTokenException.class);
+    }
+
+    @Test
+    void invalidToken() throws Exception {
+        //given
+        provider = new JwtProvider(secretKey, 100L, issuer);
+        String token = "token token";
+
+        //when then
+        assertThatThrownBy(() -> provider.parseUserId(token))
+                .isInstanceOf(InvalidTokenException.class);
+    }
+}
