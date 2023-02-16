4e263ea5f4fd4ec29ac5a999f20f7defca10fdf6
wlsh44 <k87913j@gmail.com>
2023-02-17 00:28:11 +0900

    feat: 로그인 controller 추가 [#1]

--- src/main/java/com/example/sns/auth/presentation/AuthController.java
+++ src/main/java/com/example/sns/auth/presentation/AuthController.java
@@ -1,13 +1,15 @@
 package com.example.sns.auth.presentation;
 
 import com.example.sns.auth.application.AuthService;
+import com.example.sns.auth.infrastructure.dto.GoogleAuthResponse;
+import com.example.sns.auth.presentation.dto.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -30,5 +32,10 @@ public class AuthController {
                 .status(HttpStatus.SEE_OTHER)
                 .headers(headers)
                 .build();
+    }
+
+    @GetMapping("/sign-in")
+    public ResponseEntity<TokenResponse> signIn(@RequestParam String code) {
+        return ResponseEntity.ok(authService.signIn(code));
     }
 }
