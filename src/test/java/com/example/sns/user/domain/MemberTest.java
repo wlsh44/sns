35521090d03feca0e2dbbbc46d1d6d3cebf8ebbf
wlsh44 <k87913j@gmail.com>
2023-02-17 00:26:24 +0900

    test: 테스트 코드 fixture 로 변경 [#1]

--- src/test/java/com/example/sns/user/domain/MemberTest.java
+++ src/test/java/com/example/sns/user/domain/MemberTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static com.example.sns.common.fixtures.MemberFixture.BASIC_EMAIL;
+import static com.example.sns.common.fixtures.MemberFixture.BASIC_NAME;
+import static com.example.sns.common.fixtures.MemberFixture.BASIC_NICKNAME;
+import static com.example.sns.common.fixtures.MemberFixture.BASIC_SOCIAL_ID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -17,23 +21,17 @@ class MemberTest {
     @Test
     void createUserFromInfo() throws Exception {
         //given
-        OAuthUserInfoDto userInfoDto = mock(OAuthUserInfoDto.class);
-        String email = "nickname@test.test";
-        String socialId = "12345";
-        String userName = "test";
-        given(userInfoDto.getEmail()).willReturn(email);
-        given(userInfoDto.getSocialId()).willReturn(socialId);
-        given(userInfoDto.getName()).willReturn(userName);
+        OAuthUserInfoDto userInfoDto = new OAuthUserInfoDto(BASIC_NAME, BASIC_EMAIL, BASIC_SOCIAL_ID);
 
         //when
         Member member = Member.createUserFrom(userInfoDto);
 
         //then
         assertSoftly(softAssertions -> {
-            softAssertions.assertThat(member.getInfo().getUserName()).isEqualTo(userName);
-            softAssertions.assertThat(member.getInfo().getEmail()).isEqualTo(email);
-            softAssertions.assertThat(member.getInfo().getNickName()).isEqualTo("nickname");
-            softAssertions.assertThat(member.getSocialId()).isEqualTo(socialId);
+            softAssertions.assertThat(member.getInfo().getUserName()).isEqualTo(BASIC_NAME);
+            softAssertions.assertThat(member.getInfo().getEmail()).isEqualTo(BASIC_EMAIL);
+            softAssertions.assertThat(member.getInfo().getNickName()).isEqualTo(BASIC_NICKNAME);
+            softAssertions.assertThat(member.getSocialId()).isEqualTo(BASIC_SOCIAL_ID);
         });
     }
 }
