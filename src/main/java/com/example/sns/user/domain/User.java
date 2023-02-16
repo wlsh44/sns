ba66d4e9142bdb4d5b75be0dd43e7a3dbcf99e22
wlsh44 <k87913j@gmail.com>
2023-02-16 09:38:26 +0900

    feat: id token의 정보로 유저 엔티티 만드는 기능 추가 [#1]

--- src/main/java/com/example/sns/user/domain/User.java
+++ src/main/java/com/example/sns/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.example.sns.user.domain;
 
+import com.example.sns.auth.application.dto.OAuthUserInfoDto;
 import com.example.sns.common.entity.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -28,4 +29,18 @@ public class User extends BaseTimeEntity {
     private String profileUrl;
 
     private String biography;
+
+    private User(String socialId, String userName, String nickName, String email) {
+        this.socialId = socialId;
+        this.info = new UserInfo(userName, nickName, email);
+    }
+
+    public static User createUserFrom(OAuthUserInfoDto userInfo) {
+        String nickName = getNickNameFromEmail(userInfo);
+        return new User(userInfo.getSocialId(), userInfo.getName(), nickName, userInfo.getEmail());
+    }
+
+    private static String getNickNameFromEmail(OAuthUserInfoDto userInfo) {
+        return userInfo.getEmail().split("@")[0];
+    }
 }
--- src/main/java/com/example/sns/user/domain/UserInfo.java
+++ src/main/java/com/example/sns/user/domain/UserInfo.java
@@ -1,11 +1,16 @@
 package com.example.sns.user.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Getter
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserInfo {
 
     private String userName;
