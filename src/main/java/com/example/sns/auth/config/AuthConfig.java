9d1acceca12f13bccb503121452355cdd0c2c380
wlsh44 <k87913j@gmail.com>
2023-02-17 01:01:22 +0900

    feat: 유저 인증 인터셉터 기능 추가 [#1]

--- src/main/java/com/example/sns/auth/config/AuthConfig.java
+++ src/main/java/com/example/sns/auth/config/AuthConfig.java
@@ -1,18 +1,38 @@
 package com.example.sns.auth.config;
 
+import com.example.sns.auth.presentation.AuthArgumentResolver;
+import com.example.sns.auth.presentation.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
-public class AuthConfig {
+public class AuthConfig implements WebMvcConfigurer {
 
     private final AuthProperties properties;
+    private final AuthInterceptor authInterceptor;
 
     @Bean
     public WebClient oauthProvider() {
         return WebClient.create(properties.getTokenUrl());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/auth/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthArgumentResolver());
     }
 }
---
+++ src/main/java/com/example/sns/auth/presentation/AuthArgumentResolver.java
@@ -0,0 +1,24 @@
+package com.example.sns.auth.presentation;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.example.sns.auth.presentation.AuthInterceptor.MEMBER_ID_KEY;
+
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Authenticated.class) && parameter.getParameterType().isAssignableFrom(Long.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return (Long) ((HttpServletRequest) webRequest.getNativeRequest()).getAttribute(MEMBER_ID_KEY);
+    }
+}
