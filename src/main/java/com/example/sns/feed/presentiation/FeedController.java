0115327d4907eb0efe736ed8c0256f7b69d41958
wlsh44 <k87913j@gmail.com>
2023-02-19 23:33:40 +0900

    style: 안 쓰는 코드 제거 [#3]

--- src/main/java/com/example/sns/feed/presentiation/FeedController.java
+++ src/main/java/com/example/sns/feed/presentiation/FeedController.java
@@ -6,7 +6,6 @@ import com.example.sns.feed.application.dto.FeedUploadRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
