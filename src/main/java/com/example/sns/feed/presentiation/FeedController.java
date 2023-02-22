291f6ac416506454e4ea1dc87d1fa5350d87f861
wlsh44 <k87913j@gmail.com>
2023-02-23 02:10:30 +0900

    feat: 피드 수정 controller 추가

--- src/main/java/com/example/sns/feed/presentiation/FeedController.java
+++ src/main/java/com/example/sns/feed/presentiation/FeedController.java
@@ -2,9 +2,11 @@ package com.example.sns.feed.presentiation;
 
 import com.example.sns.auth.presentation.Authenticated;
 import com.example.sns.feed.application.FeedService;
+import com.example.sns.feed.application.dto.FeedUpdateRequest;
 import com.example.sns.feed.application.dto.FeedUploadRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -25,6 +27,15 @@ public class FeedController {
                                            @RequestPart(value = "dto") FeedUploadRequest request,
                                            @RequestPart List<MultipartFile> feedImages) {
         feedService.uploadFeed(memberId, request, feedImages);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{feedId}")
+    public ResponseEntity<Void> updateFeed(@Authenticated Long memberId,
+                                           @PathVariable Long feedId,
+                                           @RequestPart(value = "dto") FeedUpdateRequest request,
+                                           @RequestPart List<MultipartFile> feedImages) {
+        feedService.updateFeed(memberId, feedId, request, feedImages);
         return ResponseEntity.ok().build();
     }
 }
