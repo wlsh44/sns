f43556386f2b786022d323a3a6a7640cf9ed3fc3
wlsh44 <k87913j@gmail.com>
2023-04-04 17:36:03 +0900

    test: 댓글 삭제 service 테스트 코드 추가

--- src/test/java/com/example/sns/post/application/CommentServiceTest.java
+++ src/test/java/com/example/sns/post/application/CommentServiceTest.java
@@ -1,36 +1,34 @@
 package com.example.sns.post.application;
 
+import com.example.sns.common.support.ServiceTest;
 import com.example.sns.post.application.dto.NewCommentRequest;
 import com.example.sns.post.domain.Comment;
 import com.example.sns.post.domain.CommentRepository;
 import com.example.sns.post.domain.Post;
 import com.example.sns.post.domain.PostRepository;
+import com.example.sns.post.exception.CommentNotFoundException;
 import com.example.sns.post.exception.EmptyCommentException;
+import com.example.sns.post.exception.NotCommentAuthorException;
 import com.example.sns.post.exception.PostNotFoundException;
-import com.example.sns.member.domain.Member;
-import com.example.sns.member.domain.MemberRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static com.example.sns.common.fixtures.CommentFixture.BASIC_COMMENT_CONTENT;
 import static com.example.sns.common.fixtures.CommentFixture.getBasicCommentRequest;
 import static com.example.sns.common.fixtures.CommentFixture.getEmptyContentCommentRequest;
-import static com.example.sns.common.fixtures.FeedFixture.getBasicFeed;
-import static com.example.sns.common.fixtures.MemberFixture.getBasicMember;
+import static com.example.sns.common.fixtures.PostFixture.getBasicPost;
+import static com.example.sns.common.fixtures.PostFixture.getBasicPostImages;
+import static com.example.sns.common.fixtures.PostFixture.getBasicUploadRequest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest
-class CommentServiceTest {
+class CommentServiceTest extends ServiceTest {
 
     @Autowired
     CommentService commentService;
-
-    @Autowired
-    MemberRepository memberRepository;
 
     @Autowired
     PostRepository postRepository;
@@ -38,20 +36,13 @@ class CommentServiceTest {
     @Autowired
     CommentRepository commentRepository;
 
-    Member member;
-
     Post post;
-
-    @BeforeEach
-    void init() {
-        member = memberRepository.save(getBasicMember());
-        post = postRepository.save(getBasicFeed(member));
-    }
 
     @Test
     @DisplayName("올바른 댓글을 생성해야 함")
     void create() throws Exception {
         //given
+        post = postRepository.save(getBasicPost(member));
         NewCommentRequest request = getBasicCommentRequest(post.getId());
 
         //when
@@ -61,13 +52,14 @@ class CommentServiceTest {
         Comment comment = commentRepository.findAll().get(0);
         assertThat(comment.getContent()).isEqualTo(BASIC_COMMENT_CONTENT);
         assertThat(comment.getPost().getId()).isEqualTo(post.getId());
-        assertThat(comment.getMember().getId()).isEqualTo(member.getId());
+        assertThat(comment.getAuthor().getId()).isEqualTo(member.getId());
     }
 
     @Test
     @DisplayName("댓글 내용이 비었을 경우 예외가 발생해야 함")
     void create_emptyContent() throws Exception {
         //given
+        post = postRepository.save(getBasicPost(member));
         NewCommentRequest request = getEmptyContentCommentRequest(post.getId());
 
         //when then
@@ -79,11 +71,53 @@ class CommentServiceTest {
     @DisplayName("없는 피드에 댓글을 달 경우 예외가 발생해야 함")
     void create_feedNotFound() throws Exception {
         //given
+        post = postRepository.save(getBasicPost(member));
         Long notExistFeedId = 9999L;
         NewCommentRequest request = getBasicCommentRequest(notExistFeedId);
 
         //when then
         assertThatThrownBy(() -> commentService.createComment(member.getId(), request))
                 .isInstanceOf(PostNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("댓글이 삭제 되어야 함")
+    void deleteCommentTest() throws Exception {
+        //given
+        post = postRepository.save(getBasicPost(member));
+        commentService.createComment(member.getId(), getBasicCommentRequest(post.getId()));
+
+        //when
+        commentService.deleteComment(member.getId(), 1L);
+
+        //then
+        List<Comment> comments = commentRepository.findAll();
+        assertThat(comments).hasSize(0);
+    }
+
+    @Test
+    @DisplayName("없는 댓글일 경우 예외가 발생해야 함")
+    void deleteCommentTest_commentNotFound() throws Exception {
+        //given
+        post = postRepository.save(getBasicPost(member));
+        Long notExistId = 999L;
+        commentService.createComment(member.getId(), getBasicCommentRequest(post.getId()));
+
+        //when then
+        assertThatThrownBy(() -> commentService.deleteComment(member.getId(), notExistId))
+                .isInstanceOf(CommentNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("댓글 작성자가 아닐 경우 예외가 발생해야 함")
+    void deleteCommentTest_notAuthor() throws Exception {
+        //given
+        post = postRepository.save(getBasicPost(member));
+        Long notAuthorId = 999L;
+        commentService.createComment(member.getId(), getBasicCommentRequest(post.getId()));
+
+        //when then
+        assertThatThrownBy(() -> commentService.deleteComment(notAuthorId, 1L))
+                .isInstanceOf(NotCommentAuthorException.class);
     }
 }
