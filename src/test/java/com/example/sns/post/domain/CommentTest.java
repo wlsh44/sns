162508fe44abbe1dc7d2bf7485e8e69ada6a3513
wlsh44 <k87913j@gmail.com>
2023-04-04 16:55:57 +0900

    test: 댓글 작성자 검증 테스트 코드 추가

--- src/test/java/com/example/sns/post/domain/CommentTest.java
+++ src/test/java/com/example/sns/post/domain/CommentTest.java
@@ -2,6 +2,7 @@ package com.example.sns.post.domain;
 
 import com.example.sns.member.domain.Member;
 import com.example.sns.post.exception.EmptyCommentException;
+import com.example.sns.post.exception.NotCommentOwnerException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,8 +13,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class CommentTest {
@@ -43,5 +46,30 @@ class CommentTest {
         //when then
         assertThatThrownBy(() -> Comment.createComment(member, content))
                 .isInstanceOf(EmptyCommentException.class);
+    }
+
+    @Test
+    @DisplayName("댓글 작성자라면 아무 일도 일어나면 안 됨")
+    void validateOwnerTest() throws Exception {
+        //given
+        Long memberId = 1L;
+        Comment comment = Comment.createComment(member, "content");
+        given(member.getId()).willReturn(memberId);
+
+        //when then
+        assertThatNoException().isThrownBy(() -> comment.validateIsOwner(memberId));
+    }
+
+    @Test
+    @DisplayName("댓글 작성자가 아니라면 예외가 발생해야 함")
+    void validateOwnerTest_notOwner() throws Exception {
+        //given
+        Long notOwnerId = 999L;
+        Comment comment = Comment.createComment(member, "content");
+        given(member.getId()).willReturn(1L);
+
+        //when then
+        assertThatThrownBy(() -> comment.validateIsOwner(notOwnerId))
+                .isInstanceOf(NotCommentOwnerException.class);
     }
 }
