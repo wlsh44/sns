039cf143b6bbda5bd85b9584650a83da8e2cf56e
wlsh44 <k87913j@gmail.com>
2023-04-04 21:44:29 +0900

    test: 댓글 삭제 controller 테스트 코드 추가

--- src/test/java/com/example/sns/post/presentiation/CommentControllerTest.java
+++ src/test/java/com/example/sns/post/presentiation/CommentControllerTest.java
@@ -3,7 +3,9 @@ package com.example.sns.post.presentiation;
 import com.example.sns.common.support.MockControllerTest;
 import com.example.sns.common.exception.dto.ErrorResponse;
 import com.example.sns.post.application.dto.NewCommentRequest;
+import com.example.sns.post.exception.CommentNotFoundException;
 import com.example.sns.post.exception.EmptyCommentException;
+import com.example.sns.post.exception.NotCommentAuthorException;
 import com.example.sns.post.exception.PostNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.not;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -70,5 +74,41 @@ class CommentControllerTest extends MockControllerTest {
                         .content(mapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().json(mapper.writeValueAsString(expect)));
+    }
+
+    @Test
+    @DisplayName("댓글 삭제에 성공할 경우 200을 응답해야 함")
+    void deleteTest() throws Exception {
+        //given
+        doNothing().when(commentService)
+                .deleteComment(any(), any());
+
+        //when then
+        mockMvc.perform(delete("/comments/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("댓글이 없을 경우 400을 응답해야 함")
+    void deleteTest_commentNotFound() throws Exception {
+        //given
+        doThrow(new CommentNotFoundException(1L)).when(commentService)
+                .deleteComment(any(), any());
+
+        //when then
+        mockMvc.perform(delete("/comments/1"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("댓글 작성자가 아닐 경우 400을 응답해야 함")
+    void deleteTest_notAuthor() throws Exception {
+        //given
+        doThrow(new NotCommentAuthorException()).when(commentService)
+                .deleteComment(any(), any());
+
+        //when then
+        mockMvc.perform(delete("/comments/1"))
+                .andExpect(status().isBadRequest());
     }
 }
