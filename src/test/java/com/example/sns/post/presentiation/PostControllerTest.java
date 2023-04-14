691a286c1f4c3dab0cd1eda33392145a92d06f25
wlsh44 <k87913j@gmail.com>
2023-04-14 16:07:52 +0900

    test: 게시글 조회 테스트 코드 추가

--- src/test/java/com/example/sns/post/presentiation/PostControllerTest.java
+++ src/test/java/com/example/sns/post/presentiation/PostControllerTest.java
@@ -1,28 +1,38 @@
 package com.example.sns.post.presentiation;
 
 import com.example.sns.common.support.MockControllerTest;
+import com.example.sns.member.exception.MemberNotFoundException;
+import com.example.sns.post.application.dto.PostResponse;
 import com.example.sns.post.exception.PostNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 
 
+import java.time.LocalDate;
+import java.util.List;
+
 import static com.example.sns.common.fixtures.AuthFixture.ACCESS_TOKEN;
+import static com.example.sns.common.fixtures.MemberFixture.BASIC_NICKNAME;
+import static com.example.sns.common.fixtures.PostFixture.BASIC_POST_CONTENT;
 import static com.example.sns.common.fixtures.PostFixture.BASIC_POST_IMAGE1;
 import static com.example.sns.common.fixtures.PostFixture.BASIC_POST_IMAGE2;
 import static com.example.sns.common.fixtures.PostFixture.BASIC_POST_UPDATE_REQUEST_MULTIPART;
 import static com.example.sns.common.fixtures.PostFixture.BASIC_POST_UPLOAD_REQUEST_MULTIPART;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class PostControllerTest extends MockControllerTest {
 
     @Test
-    @DisplayName("피드 업로드를 성공하면 200 응답을 함")
+    @DisplayName("게시글 업로드를 성공하면 200 응답을 함")
     void uploadTest() throws Exception {
         mockMvc.perform(multipart("/posts")
                         .file(BASIC_POST_IMAGE1)
@@ -34,7 +44,7 @@ class PostControllerTest extends MockControllerTest {
     }
 
     @Test
-    @DisplayName("피드를 수정하면 200 응답을 함")
+    @DisplayName("게시글를 수정하면 200 응답을 함")
     void updateTest() throws Exception {
         mockMvc.perform(multipart("/posts/1")
                         .file(BASIC_POST_IMAGE1)
@@ -45,7 +55,7 @@ class PostControllerTest extends MockControllerTest {
     }
 
     @Test
-    @DisplayName("피드 삭제에 성공하면 200 응답을 함")
+    @DisplayName("게시글 삭제에 성공하면 200 응답을 함")
     void deleteTest() throws Exception {
         mockMvc.perform(delete("/posts/1")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN))
@@ -53,7 +63,7 @@ class PostControllerTest extends MockControllerTest {
     }
 
     @Test
-    @DisplayName("피드 삭제에 성공하면 200 응답을 함")
+    @DisplayName("게시글 삭제에 성공하면 200 응답을 함")
     void deleteTest_feedNotFound() throws Exception {
         //given
         doThrow(new PostNotFoundException(1L))
@@ -61,6 +71,44 @@ class PostControllerTest extends MockControllerTest {
                 .deletePost(anyLong(), anyLong());
 
         mockMvc.perform(delete("/posts/1")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("게시글 조회에 성공하면 데이터와 200 응답을 함")
+    void findPostTest() throws Exception {
+        //given
+        PostResponse response = new PostResponse(1L, BASIC_NICKNAME, List.of("url"), 0, BASIC_POST_CONTENT, LocalDate.now(), false);
+        given(postService.findPost(any(), any()))
+                .willReturn(response);
+
+        mockMvc.perform(get("/posts/1")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN))
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(response)));
+    }
+
+    @Test
+    @DisplayName("유저가 없을 때 게시글 조회를 하는 경우 400 응답을 함")
+    void findPostTest_memberNotFound() throws Exception {
+        //given
+        given(postService.findPost(any(), any()))
+                .willThrow(new MemberNotFoundException(1L));
+
+        mockMvc.perform(get("/posts/1")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("게시글이 없을 때 없을 때 게시글 조회를 하는 경우 400 응답을 함")
+    void findPostTest_postNotFound() throws Exception {
+        //given
+        given(postService.findPost(any(), any()))
+                .willThrow(new PostNotFoundException(1L));
+
+        mockMvc.perform(get("/posts/1")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN))
                 .andExpect(status().isBadRequest());
     }
