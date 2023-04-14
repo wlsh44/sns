ffb1edaeb7fdadc02567ac95f111452ffb0608f8
wlsh44 <k87913j@gmail.com>
2023-04-14 13:56:48 +0900

    feat: 좋아요 취소 controller 추가

--- src/main/java/com/example/sns/post/presentiation/LikeController.java
+++ src/main/java/com/example/sns/post/presentiation/LikeController.java
@@ -4,6 +4,7 @@ import com.example.sns.auth.presentation.Authenticated;
 import com.example.sns.post.application.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,12 @@ public class LikeController {
     @PostMapping("/{postId}/like")
     public ResponseEntity<Void> like(@Authenticated Long memberId, @PathVariable Long postId) {
         likeService.like(memberId, postId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{postId}/like")
+    public ResponseEntity<Void> cancelLike(@Authenticated Long memberId, @PathVariable Long postId) {
+        likeService.cancelLike(memberId, postId);
         return ResponseEntity.ok().build();
     }
 }
