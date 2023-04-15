10e4626afd7abca41cd90f60b036a697e58d145d
wlsh44 <k87913j@gmail.com>
2023-04-15 19:04:01 +0900

    feat: 최신 피드 조회 controller 추가

--- src/main/java/com/example/sns/post/presentiation/FeedController.java
+++ src/main/java/com/example/sns/post/presentiation/FeedController.java
@@ -3,6 +3,7 @@ package com.example.sns.post.presentiation;
 import com.example.sns.auth.presentation.Authenticated;
 import com.example.sns.post.application.FeedService;
 import com.example.sns.post.presentiation.dto.MyFeedResponse;
+import com.example.sns.post.presentiation.dto.RecentFeedResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -21,6 +22,12 @@ public class FeedController {
     @GetMapping("/me")
     public ResponseEntity<MyFeedResponse> findMyFeed(@Authenticated Long memberId, @PageableDefault(size = 5) Pageable pageable) {
         MyFeedResponse response = feedService.findMyFeed(memberId, pageable);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/timeline")
+    public ResponseEntity<RecentFeedResponse> findRecentFeed(Pageable pageable) {
+        RecentFeedResponse response = feedService.findRecentFeed(pageable);
         return ResponseEntity.ok(response);
     }
 }
