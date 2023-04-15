48e126ea46e8a2885a7152196643db104f73d3ce
wlsh44 <k87913j@gmail.com>
2023-04-15 19:04:10 +0900

    test: 최신 피드 조회 테스트 코드 추가

--- src/test/java/com/example/sns/post/presentiation/FeedControllerTest.java
+++ src/test/java/com/example/sns/post/presentiation/FeedControllerTest.java
@@ -3,6 +3,8 @@ package com.example.sns.post.presentiation;
 import com.example.sns.common.support.MockControllerTest;
 import com.example.sns.post.application.dto.PostResponse;
 import com.example.sns.post.presentiation.dto.MyFeedResponse;
+import com.example.sns.post.presentiation.dto.RecentFeedPostResponse;
+import com.example.sns.post.presentiation.dto.RecentFeedResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +15,7 @@ import java.util.List;
 import static com.example.sns.common.fixtures.AuthFixture.ACCESS_TOKEN;
 import static com.example.sns.common.fixtures.MemberFixture.BASIC_NICKNAME;
 import static com.example.sns.common.fixtures.PostFixture.BASIC_POST_CONTENT;
+import static com.example.sns.common.fixtures.PostFixture.POST_IMAGE_PATH1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,6 +36,20 @@ class FeedControllerTest extends MockControllerTest {
                 .willReturn(response);
 
         mockMvc.perform(get("/feed/me")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN))
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(response)));
+    }
+
+    @Test
+    @DisplayName("최신 피드 조회에 성공하면 데이터와 200 응답을 함")
+    void findRecentFeedTest() throws Exception {
+        //given
+        RecentFeedResponse response = new RecentFeedResponse(List.of(new RecentFeedPostResponse(1L, POST_IMAGE_PATH1)), false, 0);
+        given(feedService.findRecentFeed(any()))
+                .willReturn(response);
+
+        mockMvc.perform(get("/feed/timeline")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN))
                 .andExpect(status().isOk())
                 .andExpect(content().json(mapper.writeValueAsString(response)));
