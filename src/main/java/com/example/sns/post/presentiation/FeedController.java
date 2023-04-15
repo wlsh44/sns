8933ec2ffd2d0dfd544db4f0dee1f95524c8ebfe
wlsh44 <k87913j@gmail.com>
2023-04-15 18:13:15 +0900

    feat: 유저 피드 조회 controller 추가

---
+++ src/main/java/com/example/sns/post/presentiation/FeedController.java
@@ -0,0 +1,26 @@
+package com.example.sns.post.presentiation;
+
+import com.example.sns.auth.presentation.Authenticated;
+import com.example.sns.post.application.FeedService;
+import com.example.sns.post.presentiation.dto.MyFeedResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("feed")
+@RequiredArgsConstructor
+public class FeedController {
+
+    private final FeedService feedService;
+
+    @GetMapping("/me")
+    public ResponseEntity<MyFeedResponse> findMyFeed(@Authenticated Long memberId, @PageableDefault(size = 5) Pageable pageable) {
+        MyFeedResponse response = feedService.findMyFeed(memberId, pageable);
+        return ResponseEntity.ok(response);
+    }
+}
