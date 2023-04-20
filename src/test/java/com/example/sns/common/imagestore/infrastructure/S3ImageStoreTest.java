fd42e2cd8e4bbaac9c52ecdbbaa62b789f9b91b5
wlsh44 <k87913j@gmail.com>
2023-04-20 20:44:08 +0900

    test: S3PostImageStore 테스트 코드 추가

---
+++ src/test/java/com/example/sns/common/imagestore/infrastructure/S3ImageStoreTest.java
@@ -0,0 +1,12 @@
+package com.example.sns.common.imagestore.infrastructure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class S3ImageStoreTest {
+
+
+}
