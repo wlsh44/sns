5632ddb65e4bf00df0408e0228e6beb211fb3f0a
wlsh44 <k87913j@gmail.com>
2023-04-21 15:49:23 +0900

    feat: 멤버 정보 조회 service 추가

--- src/main/java/com/example/sns/member/application/MemberQueryService.java
+++ src/main/java/com/example/sns/member/application/MemberQueryService.java
@@ -3,6 +3,7 @@ package com.example.sns.member.application;
 import com.example.sns.member.domain.Member;
 import com.example.sns.member.domain.MemberRepository;
 import com.example.sns.member.exception.MemberNotFoundException;
+import com.example.sns.member.presentation.dto.MemberInfoResponse;
 import com.example.sns.member.presentation.dto.MemberProfileResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,11 +17,21 @@ public class MemberQueryService {
     private final MemberRepository memberRepository;
 
     public MemberProfileResponse getProfile(Long memberId, String username) {
-        Member loginMember = memberRepository.findById(memberId)
-                .orElseThrow(() -> new MemberNotFoundException(memberId));
+        Member loginMember = getMember(memberId);
         Member profileMember = memberRepository.findByInfoNickname(username)
                 .orElseThrow(() -> new MemberNotFoundException(username));
 
         return MemberProfileResponse.from(profileMember, profileMember.isFollower(loginMember));
+    }
+
+    public MemberInfoResponse getMemberInfo(Long memberId) {
+        Member member = getMember(memberId);
+
+        return MemberInfoResponse.from(member);
+    }
+
+    private Member getMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(memberId));
     }
 }
