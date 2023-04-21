59affef8379d39b90cf27e89fcb7f9302a9d94b9
wlsh44 <k87913j@gmail.com>
2023-04-21 17:39:32 +0900

    feat: s3프로필 저장 기능 추가

---
+++ src/main/java/com/example/sns/member/infrastructure/S3ProfileImageStore.java
@@ -0,0 +1,34 @@
+package com.example.sns.member.infrastructure;
+
+import com.example.sns.common.imagestore.ImageStore;
+import com.example.sns.common.imagestore.exception.InvalidImageException;
+import com.example.sns.member.application.ProfileImageStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class S3ProfileImageStore implements ProfileImageStore {
+
+    private final String storePath;
+    private final ImageStore imageStore;
+
+    public S3ProfileImageStore(@Value("${cloud.aws.s3.store-path.profile}") String storePath,
+                               ImageStore imageStore) {
+        this.storePath = storePath;
+        this.imageStore = imageStore;
+    }
+
+    @Override
+    public String saveProfileImage(MultipartFile image) {
+        validateEmptyImages(image);
+
+        return imageStore.saveImage(image, storePath);
+    }
+
+    private void validateEmptyImages(MultipartFile image) {
+        if (image.isEmpty()) {
+            throw new InvalidImageException();
+        }
+    }
+}
