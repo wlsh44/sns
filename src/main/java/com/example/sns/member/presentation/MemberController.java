0fa56a65e596088a13fa425cdcfdbcd0d6ea67f6
wlsh44 <k87913j@gmail.com>
2023-04-21 18:22:36 +0900

    test: 유저 정보 수정 controller 추가

--- src/main/java/com/example/sns/member/presentation/MemberController.java
+++ src/main/java/com/example/sns/member/presentation/MemberController.java
@@ -1,15 +1,21 @@
 package com.example.sns.member.presentation;
 
 import com.example.sns.auth.presentation.Authenticated;
+import com.example.sns.member.application.MemberCommandService;
 import com.example.sns.member.application.MemberQueryService;
+import com.example.sns.member.application.dto.MemberUpdateRequest;
 import com.example.sns.member.presentation.dto.MemberInfoResponse;
 import com.example.sns.member.presentation.dto.MemberProfileResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/members")
@@ -17,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberQueryService memberQueryService;
+    private final MemberCommandService memberCommandService;
 
     @GetMapping("/profile")
     public ResponseEntity<MemberProfileResponse> getProfile(@Authenticated Long memberId, @RequestParam String username) {
@@ -30,5 +37,15 @@ public class MemberController {
         MemberInfoResponse response = memberQueryService.getMemberInfo(memberId);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/me")
+    public ResponseEntity<Void> updateMember(
+            @Authenticated Long memberId,
+            @RequestPart("body") MemberUpdateRequest request,
+            @RequestPart("image") MultipartFile multipartFile) {
+        memberCommandService.updateMember(memberId, request, multipartFile);
+
+        return ResponseEntity.ok().build();
     }
 }
