8ee868d139429a25ffb935a3283e7b7ca56223fd
wlsh44 <k87913j@gmail.com>
2023-04-21 16:43:18 +0900

    feat: 멤버 정보 수정 기능 추가

--- src/main/java/com/example/sns/member/domain/MemberInfo.java
+++ src/main/java/com/example/sns/member/domain/MemberInfo.java
@@ -1,9 +1,11 @@
 package com.example.sns.member.domain;
 
+import com.example.sns.member.exception.InvalidNicknameException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.Embeddable;
 
@@ -18,4 +20,15 @@ public class MemberInfo {
     private String nickname;
 
     private String email;
+
+    public void updateNickname(String nickname) {
+        validateNickname(nickname);
+        this.nickname = nickname;
+    }
+
+    private void validateNickname(String nickname) {
+        if (!StringUtils.hasText(nickname)) {
+            throw new InvalidNicknameException(nickname);
+        }
+    }
 }
