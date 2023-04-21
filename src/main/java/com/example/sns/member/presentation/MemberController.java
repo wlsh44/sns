77ff4bdd3a66d0f235b04558e3644891ed00f96c
wlsh44 <k87913j@gmail.com>
2023-04-21 15:52:28 +0900

    feat: 멤버 정보 조회 controller 추가

--- src/main/java/com/example/sns/member/presentation/MemberController.java
+++ src/main/java/com/example/sns/member/presentation/MemberController.java
@@ -2,6 +2,7 @@ package com.example.sns.member.presentation;
 
 import com.example.sns.auth.presentation.Authenticated;
 import com.example.sns.member.application.MemberQueryService;
+import com.example.sns.member.presentation.dto.MemberInfoResponse;
 import com.example.sns.member.presentation.dto.MemberProfileResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,13 @@ public class MemberController {
     @GetMapping("/profile")
     public ResponseEntity<MemberProfileResponse> getProfile(@Authenticated Long memberId, @RequestParam String username) {
         MemberProfileResponse response = memberQueryService.getProfile(memberId, username);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberInfoResponse> getMemberInfo(@Authenticated Long memberId) {
+        MemberInfoResponse response = memberQueryService.getMemberInfo(memberId);
 
         return ResponseEntity.ok(response);
     }
