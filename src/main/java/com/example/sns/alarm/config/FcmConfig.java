dd9119aa0f943ad2eb93a62b69f086c2371b7a68
wlsh44 <k87913j@gmail.com>
2023-04-27 21:58:52 +0900

    chore: fcmConfig 실행 프로필 및 테스트 설정값 추가

--- src/main/java/com/example/sns/alarm/config/FcmConfig.java
+++ src/main/java/com/example/sns/alarm/config/FcmConfig.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @Slf4j
 @Configuration
+@Profile({"dev", "prod"})
 public class FcmConfig {
 
     private final String fcmPrivateKeyPath;
