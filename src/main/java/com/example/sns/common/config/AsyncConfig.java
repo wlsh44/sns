98bf230e90a58fc80c3dbe55242587f76b9c1e42
wlsh44 <k87913j@gmail.com>
2023-04-27 22:00:20 +0900

    feat: 비동기 thread pool 설정 추가

---
+++ src/main/java/com/example/sns/common/config/AsyncConfig.java
@@ -0,0 +1,22 @@
+package com.example.sns.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "asyncThreadPoolTaskExecutor")
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("CUSTOM_THREAD-");
+        executor.initialize();
+        return executor;
+    }
+}
