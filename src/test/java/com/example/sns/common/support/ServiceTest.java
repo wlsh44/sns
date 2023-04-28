491b524312bd6227678649e8c6dceb1b72ff6da6
wlsh44 <k87913j@gmail.com>
2023-04-28 12:53:16 +0900

    test: 테스트 컴파일 되도록 수정

--- src/test/java/com/example/sns/common/support/ServiceTest.java
+++ src/test/java/com/example/sns/common/support/ServiceTest.java
@@ -2,7 +2,9 @@ package com.example.sns.common.support;
 
 import com.example.sns.common.annotation.ApplicationTest;
 import com.example.sns.member.domain.MemberRepository;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import javax.persistence.EntityManager;
 
@@ -14,4 +16,7 @@ public class ServiceTest {
 
     @Autowired
     protected EntityManager em;
+
+    @MockBean
+    FirebaseMessaging firebaseMessaging;
 }
