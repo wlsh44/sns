8b06701216889529e22df1f2fdce0399f73b9da1
wlsh44 <k87913j@gmail.com>
2023-05-02 16:30:19 +0900

    test: 알람 읽기 테스트 코드 추가

---
+++ src/test/java/com/example/sns/alarm/application/AlarmCommandServiceTest.java
@@ -0,0 +1,70 @@
+package com.example.sns.alarm.application;
+
+import com.example.sns.alarm.domain.Alarm;
+import com.example.sns.alarm.domain.AlarmRepository;
+import com.example.sns.alarm.exception.AlarmNotFoundException;
+import com.example.sns.alarm.exception.NotAlarmReceiverException;
+import com.example.sns.common.support.ServiceTest;
+import com.example.sns.member.domain.Member;
+import com.example.sns.member.domain.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.example.sns.common.fixtures.AlarmFixture.getFollowAlarm;
+import static com.example.sns.common.fixtures.MemberFixture.getBasicMember;
+import static com.example.sns.common.fixtures.MemberFixture.getBasicMember2;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class AlarmCommandServiceTest extends ServiceTest {
+
+    @Autowired
+    AlarmCommandService alarmCommandService;
+
+    @Autowired
+    AlarmRepository alarmRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("알람을 읽어야 함")
+    void readTest() throws Exception {
+        //given
+        Member member = memberRepository.save(getBasicMember());
+        Alarm alarm = alarmRepository.save(getFollowAlarm(member));
+
+        //when
+        alarmCommandService.readAlarm(member.getId(), alarm.getId());
+
+        //then
+        Alarm readAlarm = alarmRepository.findAll().get(0);
+        assertThat(readAlarm.isRead()).isTrue();
+    }
+
+    @Test
+    @DisplayName("없는 알람을 읽을 경우 예외가 발생해야 함")
+    void readTest_alarmNotFound() throws Exception {
+        //given
+        Long notExistId = 999L;
+        Member member = memberRepository.save(getBasicMember());
+
+        //when then
+        assertThatThrownBy(() -> alarmCommandService.readAlarm(member.getId(), notExistId))
+                .isInstanceOf(AlarmNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("알람을 안 받은 사람이 알람을 읽을 경우 예외가 발생해야 함")
+    void readTest_notAlarmOwner() throws Exception {
+        //given
+        Member member = memberRepository.save(getBasicMember());
+        Alarm alarm = alarmRepository.save(getFollowAlarm(member));
+        Member notAlarmReceiver = memberRepository.save(getBasicMember2());
+
+        //when then
+        assertThatThrownBy(() -> alarmCommandService.readAlarm(notAlarmReceiver.getId(), alarm.getId()))
+                .isInstanceOf(NotAlarmReceiverException.class);
+    }
+}
