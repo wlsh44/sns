596c8c96ac5567ba85faa5a0e11645705c56e687
JinHo Kim <k87913j@gmail.com>
2023-05-04 21:47:53 +0900

    chore: CI/CD 워크플로우 추가 (#71)
    * chore: ci 워크플로우 추가
    
    * chore: cd 워크플로우 추가
    
    * chore: deploy.sh 추가
    
    * chore: 도커 관련 파일 추가
    
    * chore: gitignore 추가
    
    * chore: docker 레포 이름 수정
    
    * chore: dockerfile 실행 경로 수정
    
    * Revert "chore: dockerfile 실행 경로 수정"
    
    This reverts commit 0d53422957fdabc50b597c562e1d95ba712a5b1a.
    
    * chore: dockerfile 위치 수정
    
    * chore: dockerfile 수정
    
    * fix: prod환경에서 firebasekey 외부 파일 읽도록 수정

--- src/main/java/com/example/sns/common/infrastructure/fcm/config/FcmConfig.java
+++ src/main/java/com/example/sns/common/infrastructure/fcm/config/FcmConfig.java
@@ -9,10 +9,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -21,11 +26,14 @@ public class FcmConfig {
 
     private final String fcmPrivateKeyPath;
     private final String firebaseScope;
+    private final Environment env;
 
     public FcmConfig(@Value("${fcm.key.path}") String fcmPrivateKeyPath,
-                     @Value("${fcm.key.scope}") String firebaseScope) {
+                     @Value("${fcm.key.scope}") String firebaseScope,
+                     Environment env) {
         this.fcmPrivateKeyPath = fcmPrivateKeyPath;
         this.firebaseScope = firebaseScope;
+        this.env = env;
     }
 
     @Bean
@@ -39,7 +47,7 @@ public class FcmConfig {
     }
 
     private GoogleCredentials createCredentials() {
-        try (InputStream inputStream = new ClassPathResource(fcmPrivateKeyPath).getInputStream()) {
+        try (InputStream inputStream = getInputStream()) {
             return GoogleCredentials
                     .fromStream(inputStream)
                     .createScoped(List.of(firebaseScope));
@@ -47,5 +55,12 @@ public class FcmConfig {
             log.error("Firebase 인증에 실패했습니다 e = {}", e.getMessage());
             throw new RuntimeException(e);
         }
+    }
+
+    private InputStream getInputStream() throws IOException {
+        if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
+            return new FileInputStream(fcmPrivateKeyPath);
+        }
+        return new ClassPathResource(fcmPrivateKeyPath).getInputStream();
     }
 }
