ab3944a29db15187b7e9ab2c15e99906e2d33c63
wlsh44 <k87913j@gmail.com>
2023-05-06 17:38:24 +0900

    feat: new comment valid 추가

--- src/main/java/com/example/sns/post/application/dto/NewCommentRequest.java
+++ src/main/java/com/example/sns/post/application/dto/NewCommentRequest.java
@@ -1,14 +1,21 @@
 package com.example.sns.post.application.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class NewCommentRequest {
 
+    @NotEmpty
     private Long feedId;
+
+    @NotNull
     private String content;
 }
