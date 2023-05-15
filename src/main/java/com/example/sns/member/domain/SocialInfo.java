1d931995eb47b646f9547d58179abe78cd6395cc
wlsh44 <k87913j@gmail.com>
2023-05-15 22:36:10 +0900

    refactor: member 생성시 기본 값 추가

--- src/main/java/com/example/sns/member/domain/SocialInfo.java
+++ src/main/java/com/example/sns/member/domain/SocialInfo.java
@@ -8,12 +8,15 @@ import lombok.NoArgsConstructor;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.Embeddable;
+import java.util.Optional;
 
 @Getter
 @Embeddable
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SocialInfo {
+
+    private static final String DEFAULT_PROFILE = "https://kr.object.ncloudstorage.com/sns-image-s3/profile/default.jpeg";
 
     private String username;
 
@@ -23,13 +26,15 @@ public class SocialInfo {
 
     public SocialInfo(String username) {
         this.username = username;
+        this.biography = "";
+        this.profileUrl = DEFAULT_PROFILE;
     }
 
     public void update(String username, String biography, String profilePath) {
         validateUsername(username);
         this.username = username;
-        this.biography = biography;
-        this.profileUrl = profilePath;
+        this.biography = Optional.ofNullable(biography).orElse("");
+        this.profileUrl = Optional.ofNullable(profilePath).orElse(DEFAULT_PROFILE);
     }
 
     private void validateUsername(String username) {
