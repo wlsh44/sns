ed946e3ef2fca94e82ff27ba9fbfe6dce4fccb2c
wlsh44 <k87913j@gmail.com>
2023-05-15 16:52:46 +0900

    style: 안 쓰는 코드 제거

--- src/main/java/com/example/sns/member/domain/MemberRepository.java
+++ src/main/java/com/example/sns/member/domain/MemberRepository.java
@@ -3,8 +3,6 @@ package com.example.sns.member.domain;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,7 +13,4 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByInfoUsername(String username);
     boolean existsByInfoUsername(String username);
     Slice<Member> findByInfoUsernameContaining(String username, Pageable pageable);
-
-    @Query("select m from Member m left join fetch m.devices where m.id = :memberId")
-    Optional<Member> findByIdFetchDevices(@Param("memberId") Long memberId);
 }
