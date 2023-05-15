48ee895a2ad5b5432817b6245466eccb73889649
wlsh44 <k87913j@gmail.com>
2023-05-15 16:09:13 +0900

    style: 코드 정리

--- src/main/java/com/example/sns/common/config/AsyncConfig.java
+++ src/main/java/com/example/sns/common/config/AsyncConfig.java
@@ -1,6 +1,5 @@
 package com.example.sns.common.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
