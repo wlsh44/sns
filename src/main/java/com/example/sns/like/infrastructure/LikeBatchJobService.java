21819438f622e4a051cbe7deadd709fc71ccdd68
wlsh44 <k87913j@gmail.com>
2023-06-16 23:02:53 +0900

    refactor: 배치 쿼리 수정

--- src/main/java/com/example/sns/like/infrastructure/LikeBatchJobService.java
+++ src/main/java/com/example/sns/like/infrastructure/LikeBatchJobService.java
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Service;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static com.example.sns.common.infrastructure.redis.RedisConst.LIKE_EXPIRED_SECONDS;
 
@@ -22,23 +24,29 @@ import static com.example.sns.common.infrastructure.redis.RedisConst.LIKE_EXPIRE
 @RequiredArgsConstructor
 public class LikeBatchJobService {
 
-    public static final int MEMBER_ID_INDEX = 2;
-    public static final int POST_ID_INDEX = 3;
+    private static final int PARAMETER_OFFSET = 1;
+    private static final int MEMBER_ID_INDEX = 2;
+    private static final int POST_ID_INDEX = 3;
 
     private final JdbcTemplate jdbcTemplate;
     private final RedisService redisService;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     public void updateRDB() {
         List<String> keys = findLikeKeys();
 
-        String sql = "INSERT IGNORE INTO likes (member_id, post_id) values (?, ?)";
+        String sql = "INSERT IGNORE INTO likes (member_id, post_id, created_at, last_modified_at) values (?, ?, ?, ?)";
         jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement ps, int i) throws SQLException {
                 String key = keys.get(i);
-                String[] split = key.split(":");
-                ps.setLong(1, Long.parseLong(split[MEMBER_ID_INDEX]));
-                ps.setLong(2, Long.parseLong(split[POST_ID_INDEX]));
+                Timestamp createdAt = getCreatedAt(key);
+                String[] memberIdAndPostId = key.split(":");
+
+                ps.setLong(PARAMETER_OFFSET, Long.parseLong(memberIdAndPostId[MEMBER_ID_INDEX]));
+                ps.setLong(PARAMETER_OFFSET + 1, Long.parseLong(memberIdAndPostId[POST_ID_INDEX]));
+                ps.setTimestamp(PARAMETER_OFFSET + 2, createdAt);
+                ps.setTimestamp(PARAMETER_OFFSET + 3, createdAt);
             }
 
             @Override
@@ -46,6 +54,17 @@ public class LikeBatchJobService {
                 return keys.size();
             }
         });
+    }
+
+    private Timestamp getCreatedAt(String key) {
+        String createdAt = (String) redisTemplate.opsForValue().get(key);
+        return Timestamp.valueOf(toLocalDateTime(createdAt));
+    }
+
+    private static LocalDateTime toLocalDateTime(String value) {
+        return Optional.ofNullable(value)
+                .map(LocalDateTime::parse)
+                .orElseGet(LocalDateTime::now);
     }
 
     private List<String> findLikeKeys() {
